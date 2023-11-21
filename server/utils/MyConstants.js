fe11bdeda3a79b5644763217eef5b40d2454a55a
lam2509 <nguyenduylam3002@gmail.com>
2023-11-21 23:37:18 +0700

    Create MyConstants.js

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.bpgyyux.mongodb.net',
+    DB_USER: 'lam741ty',
+    DB_PASS: 'Lam15935790',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caigicungduoc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'nguyenduylam9A14@gmail.com', // gmail service
+    EMAIL_PASS: 'jctu nreq xobz exbx'
+  };
+  module.exports = MyConstants;
